ca564831ca78fbb8f080f0aa74d7015a59fb1627
GolovinD <105558409+GolovinD@users.noreply.github.com>
2023-03-27 19:21:00 +0000

    Deploying to gh-pages from @ GolovinD/goit-react-hw-08-phonebook@101d64541083e8fd21c21361ed516fd6d64470a9 🚀

---
+++ static/js/544.9a56d90b.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[544],{9544:function(e,t,n){n.r(t),n.d(t,{default:function(){return r}});var i=n(184),o={container:{minHeight:"calc(100vh - 500px)",display:"flex",alignItems:"center",justifyContent:"center"},title:{fontWeight:500,fontSize:32,textAlign:"center"}};function r(){return(0,i.jsx)("div",{style:o.container,children:(0,i.jsx)("h1",{style:o.title,children:"Welcome to Phonebook! Please register or login."})})}}}]);
+//# sourceMappingURL=544.9a56d90b.chunk.js.map
